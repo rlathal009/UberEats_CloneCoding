178be6d3a2b44f032f70e47df7cea355850669a7
Somi Kim <rlathal009@gmail.com>
2022-01-03 01:51:36 -0800

    User Resolver

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,24 @@
 import * as common from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import{TypeOrmModule} from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ignoreElements } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { User } from './users/entities/users.entity';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @common.Module({
   imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ".dev.env"
+      envFilePath: '.dev.env',
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
@@ -17,9 +27,12 @@ import { AppService } from './app.service';
       username: 'root',
       password: 'root',
       database: 'test',
-      entities: [],
+      entities: [User],
       synchronize: true,
-    })
+    }),
+    UsersModule,
+    CommonModule,
+    RestaurantsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/users.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly users: Repository<User>,
+  ) {}
+}
